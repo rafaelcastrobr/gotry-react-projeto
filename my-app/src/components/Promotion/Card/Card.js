dfe0b370eb4288fc3db7ac0c8c128eece1e6203c
rafaelcastrobr <rafaelcastrobr@gmail.com>
2022-02-05 11:18:01 -0300

    subindo api

--- my-app/src/components/Promotion/Card/Card.js
+++ my-app/src/components/Promotion/Card/Card.js
@@ -3,7 +3,7 @@ import './Card.css';
 
 const PromotionCard = ({ promotion }) => (
   <div className="promotion-card">
-    <img src={promotion.imageUrl}  alt={promotion.title} className="promotion-card__image" />
+    <img src={promotion.imageUrl} alt={promotion.title} className="promotion-card__image" />
     <div className="promotion-card__info">
       <h1 className="promotion-card__title">{promotion.title}</h1>
       <span className="promotion-card__price">R$ {promotion.price} </span>
@@ -15,7 +15,13 @@ const PromotionCard = ({ promotion }) => (
         <div className="promotion-card__comments-count">
           {promotion.comments.length}{' '}
           {promotion.comments.length > 1 ? 'Comentários' : 'Comentário'}</div>
-        <a href={promotion.url} target="_blank" rel="noreferrer" className="promotion-card__link">IR PARA O SITE</a>
+        <a
+          href={promotion.url}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="promotion-card__link">
+          IR PARA O SITE
+        </a>
       </footer>
     </div>
   </div>
